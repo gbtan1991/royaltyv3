6e81050d23d7cb11d7dd9e9a7418960dc3552874
gbtan1991 <gilbert.tan1991@gmail.com>
2025-07-17 23:13:41 +0800

    Changing the login button into CTA

--- app/View/Components/welcome/CTAButton.php
+++ app/View/Components/welcome/CTAButton.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class LoginRegisterButton extends Component
+class CTAButton extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class LoginRegisterButton extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.welcome.login-register-button');
+        return view('components.welcome.c-t-a-button');
     }
 }
