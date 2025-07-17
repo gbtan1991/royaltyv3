8e981ba999fbdab28c15ad22218f8369b8b6bbfc
gbtan1991 <gilbert.tan1991@gmail.com>
2025-07-17 23:27:11 +0800

    Removing the  login for the mean time

--- resources/views/components/welcome/c-t-a-button.blade.php
+++ resources/views/components/welcome/c-t-a-button.blade.php
@@ -1,5 +1,6 @@
 <div class="text-center">
-  <a href="{{route('/login')}}"
+  {{-- {{route('/login')}} --}}
+  <a href="#"
   
   class="inline-block bg-[#f3f7fe] text-[#8058f3] cursor-pointer rounded-lg px-6 py-2 transition duration-300 hover:bg-[#8058f3] hover:text-white hover:shadow-[0_0_0_5px_#8058f35f]">
     Get Started
