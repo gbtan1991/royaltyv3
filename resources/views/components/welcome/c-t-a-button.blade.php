63fda8c2b0c0002182895b29a9e4077288f6780a
gilbert <gilbert.tan1991@gmail.com>
2025-07-19 03:00:48 +0800

    Added the login route link

--- resources/views/components/welcome/c-t-a-button.blade.php
+++ resources/views/components/welcome/c-t-a-button.blade.php
@@ -1,6 +1,7 @@
 <div class="text-center">
   {{-- {{route('/login')}} --}}
-  <a href="#"
+  <a href="{{ route('login') }}"
+     
   
   class="inline-block bg-[#f3f7fe] text-[#8058f3] cursor-pointer rounded-lg px-6 py-2 transition duration-300 hover:bg-[#8058f3] hover:text-white hover:shadow-[0_0_0_5px_#8058f35f]">
     Get Started
