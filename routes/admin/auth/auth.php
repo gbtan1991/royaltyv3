44ee3676ae769cbfcd08011b407bf1d85b42c719
gilbert <gilbert.tan1991@gmail.com>
2025-07-26 04:10:13 +0800

    created a prefix routes for admin calling login register and logout.

---
+++ routes/admin/auth/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\AuthController;
+
+Route::prefix('admin')->name('admin.')->group(function (){
+    
+    //Show login form
+    Route::get('/login', [AuthController::class, 'showLogin'])
+        ->name('login');
+
+    //Process Login
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+    //Show Registration form
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('showRegister');
+    
+    //Process Registration
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+    //Logout
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
