519af21c4ae9b76f3abf60053f2c17adda6b970b
gilbert <gilbert.tan1991@gmail.com>
2025-07-26 02:33:27 +0800

    Refactor the database. Adding username, account_status and admin_type

--- database/migrations/2025_07_20_131159_create_admins_table.php
+++ database/migrations/2025_07_20_131159_create_admins_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->date('date_of_birth');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('admin_type')->default('super_admin'); // Default admin type
+            $table->string('account_status')->default('pending');
             $table->rememberToken();
             $table->timestamp('last_login_at')->nullable();
             $table->timestamps();
