60f96c53083b057e85e30ec8ed1d07051e6b09c6
gilbert <gilbert.tan1991@gmail.com>
2025-07-26 04:09:00 +0800

    Added the middleware kernel and modify the admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,33 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\AdminFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $guard = 'admin';
 
-protected $fillable = [
-    'username',
-    'first_name',
-    'last_name',
-    'date_of_birth',
-    'email',
-    'password',
-    'admin_type',
-    'account_status',
-    'last_login_at',
-    
-];
+    protected $fillable = [
+        'username',
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'email',
+        'password',
+        'admin_type',
+        'account_status',
+        'last_login_at',
+    ];
 
     protected $hidden = [
         'password',
         'remember_token',
-    
     ];
 
     protected $casts = [
