1a3caea579125c5c9ca8986126caf5d39545d3c2
gilbert <gilbert.tan1991@gmail.com>
2025-08-01 05:47:26 +0800

    Created the component for sidebar.

---
+++ resources/views/components/partials/sidebar.blade.php
@@ -0,0 +1,7 @@
+<aside 
+:class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full'"
+class="sidebar fixed top-0 z-99999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 dark:border-gray-500 dark:bg-black lg:static lg:translate-x-0"
+>
+    
+
+</aside>
