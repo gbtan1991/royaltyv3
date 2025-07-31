f37cacd01bff99235ebf257481e3ddd3e640ff3f
gilbert <gilbert.tan1991@gmail.com>
2025-08-01 05:47:10 +0800

    Created the component for header.

---
+++ resources/views/components/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header x-data="{ menuToggle: false }"
+    class="sticky top-0 z-99999 flex w-full border-gray-200 bg-white lg:border-b dark:border-gray-800 dark:bg-gray-900">
+    <div class="flex grow flex-col items-center justify-between lg:flex-row lg:px-6">
+        <div
+            class="flex w-full items-center justify-between gap-2 border-b border-gray-200 px-3 py-3 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4 dark:border-gray-800">
+
+      @include('components.partials.header.hamburger-button')
+
+      @include('components.partials.header.mobile.app-logo')
+
+      @include('components.partials.header.mobile.app-nav')
+
+      @include('components.partials.header.mobile.menu-toggle')
+
+        </div>
+    </div>
+
+
+</header>
