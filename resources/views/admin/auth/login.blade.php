4c956f7133bf75c1b0cd77ba793fd26f306d40c2
gilbert <gilbert.tan1991@gmail.com>
2025-09-05 07:25:22 +0800

    Add login Blade view with form labels for email and password

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.guest-layout>
+<x-partials.forms.form-label for="email" required>Email</x-partials.forms.form-label>
+<x-partials.forms.form-label for="password">Password</x-partials.forms.form-label>
+
+<p class="text-red-500">test</p>
+</x-layouts.guest-layout>
