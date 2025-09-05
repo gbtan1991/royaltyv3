aabf14bc85e6cd6f7804872a14abe99c90e3186d
gbtan23 <gilbert.tan1991@gmail.com>
2025-09-05 20:07:01 +0800

    Add required prop to form label component for conditional asterisk display

--- resources/views/components/partials/form/form-label.blade.php
+++ resources/views/components/partials/form/form-label.blade.php
@@ -1,6 +1,8 @@
-
+@props(['required' => false])
 
 <label {{ $attributes->merge(['class' => 'mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400']) }}>
     {{ $slot }}
-  
+    @if($required)
+    <span class="text-red-500">*</span>
+    @endif
 </label>
