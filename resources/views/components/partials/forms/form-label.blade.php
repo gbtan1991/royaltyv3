4ab74f18fa0433e42e0ecf79cf37926ef1f72bc9
gbtan23 <gilbert.tan1991@gmail.com>
2025-09-05 16:04:41 +0800

    Remove unused form component files and Blade views

--- resources/views/components/partials/forms/form-label.blade.php
+++
@@ -1,8 +0,0 @@
-<label {{ $attributes->merge(['class' => 'mb-1.5 block text-sm font-medium text-gray-700'])}}>
-    {{ $slot }}
-
-    @if ($required ?? false)
-        <span class="text-red-500">*</span>
-    @endif
-
-</label>
