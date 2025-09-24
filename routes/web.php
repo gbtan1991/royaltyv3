c1d3ecee7e0c7a9d6157642f3ab7a86b7dd97283
gilbert <gilbert.tan1991@gmail.com>
2025-09-24 22:44:58 +0800

    Add route for AdminController to manage admin resources

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('admins', AdminController::class);
