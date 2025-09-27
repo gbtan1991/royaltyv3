c16b12f0ada9893216602f23c5f3a1bcb986f455
gilbert <gilbert.tan1991@gmail.com>
2025-09-27 13:02:06 +0800

    Add admin authentication routes and protect dashboard with middleware

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,19 @@ Route::get('/', function () {
 });
 
 
-Route::resource('admin', AdminController::class);
+
+// Auth Routes for Admin
+Route::get('admin/auth/login', [App\Http\Controllers\Auth\AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('admin/auth/login', [App\Http\Controllers\Auth\AdminAuthController::class, 'login'])->name('admin.login.submit');
+Route::post('admin/auth/logout', [App\Http\Controllers\Auth\AdminAuthController::class, 'logout'])->name('admin.logout');
+
+
+// Protecting dashboard and crud routes
+Route::middleware('auth:admin')->group(function(){
+    Route::get('/dashboard', function() {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::resource('admin', AdminController::class);
+});
+
+
