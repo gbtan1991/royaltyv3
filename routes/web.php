347ba649274a1e989527b5ffc0c86deff2ce94a3
gilbert <gilbert.tan1991@gmail.com>
2025-09-29 00:49:38 +0800

    Update home route to redirect to admin login view

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.auth.login');
 });
 
 
