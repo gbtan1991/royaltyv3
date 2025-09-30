00b1570a43292e778d64efdc830111f47a1b077c
gbtan23 <gilbert.tan1991@gmail.com>
2025-09-30 19:59:00 +0800

    Refactor form inputs in admin create view to use reusable components for consistency

--- resources/views/Admin/create.blade.php
+++ resources/views/Admin/create.blade.php
@@ -8,52 +8,47 @@
 
         <div>
             <x-form-label for="username" value="Username" class="block font-medium" />
-            <input type="text" name="username" class="w-full border rounded p-2" value="{{ old('username') }}">
-            @error('username') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+            <x-form-input name="username" class="w-full border rounded p-2" value="{{ old('username') }}" />
         </div>
 
         <div>
             <x-form-label for="password" value="Password" class="block font-medium" />
-            <input type="password" name="password" class="w-full border rounded p-2">
-            @error('password') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+            <x-form-input type="password" name="password" class="w-full border rounded p-2" />
         </div>
 
         <div>
             <x-form-label for="confirm_password" value="Confirm Password" class="block font-medium" />
-            <input type="password" name="password_confirmation" class="w-full border rounded p-2">
-        </div>
+            <x-form-input type="password" name="confirm_password" class="w-full border rounded p-2" />
+            </div>
 
         <div>
             <x-form-label for="first_name" value="First Name" class="block font-medium" />
-            <input type="text" name="first_name" class="w-full border rounded p-2" value="{{ old('first_name') }}">
-            @error('first_name') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+            <x-form-input name="first_name" class="w-full border rounded p-2" value="{{ old('first_name') }}" />
         </div>
 
         <div>
             <x-form-label for="last_name" value="Last Name" class="block font-medium" />
-            <input type="text" name="last_name" class="w-full border rounded p-2" value="{{ old('last_name') }}">
-            @error('last_name') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+            <x-form-input name="last_name" class="w-full border rounded p-2" value="{{ old('last_name') }}" />
         </div>
 
         <div>
             <x-form-label for="birthdate" value="Birthdate" class="block font-medium" />
-            <input type="date" name="birthdate" class="w-full border rounded p-2" value="{{ old('birthdate') }}">
+            <x-form-input type="date" name="birthdate" class="w-full border rounded p-2" value="{{ old('birthdate') }}" />
         </div>
 
         <div>
             <x-form-label for="account_type" value="Account Type" class="block font-medium" />
-            <select name="account_type" class="w-full border rounded p-2">
-                <option value="admin">Admin</option>
-                <option value="superadmin">Super Admin</option>
-            </select>
+            <x-form-select name="account_type" :options="['superadmin' => 'Super Admin', 'admin' => 'Admin']" value="user" class="" required />
+            
         </div>
 
         <div>
             <x-form-label for="avatar" value="Avatar" class="block font-medium" />
-            <input type="file" name="avatar" class="w-full border rounded p-2">
+            <x-form-input type="file" name="avatar" class="w-full border rounded p-2" />
         </div>
 
         <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">Create</button>
+        <a href="{{ route('admin.index')}}">Back to Admin List</a>
     </form>
 </div>
     
