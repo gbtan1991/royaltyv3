96e0f8c11217f6718f514248f2bc8610088fb235
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-06 20:08:42 +0800

    Created a reusable component sidebarHeader that will be included in the sidebar

---
+++ resources/views/components/partials/sidebar-header.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar-header flex items-center gap-2 pt-8 pb-7">
+
+    <a href="{{ route('dashboard')}}">
+        <span class="logo">
+            <img src="{{ asset('assets/images/royalty-logo-light.png')}}" alt="logo" class="ml-3"/>
+        </span>
+    </a>
+
+</div>
