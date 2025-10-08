c381b28d51d1b13b6e54e596e8f8921241bb4576
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-08 20:11:19 +0800

    Refactor the tailwindcss for the header and test if this appears under the proper structure.

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,5 +1,5 @@
-<header class="sticky top-0  bg-white shadow z-99999">
-    this is the header
+<header class=" sidebar sticky top-0 z-99999 flex w-full border-gray-200 bg-white lg:border-b">
+    
 
 
 </header>
