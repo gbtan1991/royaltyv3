aee0beac772a17954074daeba4898eaf81424810
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-10 20:04:08 +0800

    Added the new logo for the minimized version of sidebar

--- resources/views/components/partials/sidebar-header.blade.php
+++ resources/views/components/partials/sidebar-header.blade.php
@@ -6,6 +6,11 @@
         <span class="logo" :class="sidebarToggle ? 'hidden': ''">
             <img src="{{ asset('assets/images/royalty-logo-light.png')}}" alt="logo" class="ml-3"/>
         </span>
+
+        <img 
+        src={{ asset('assets/images/logo.png')}} 
+        alt="logo"
+        :class="sidebarToggle ? '' : 'hidden'"/>
     </a>
 
 </div>
