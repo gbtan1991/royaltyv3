fef13322ecbd393cd882f5182a7729fad1a99801
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-10 20:04:29 +0800

    Added the functionality of the sidebar to minimize it.

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -1,4 +1,7 @@
-<aside class="fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 lg:static lg:translate-x-0">
+<aside
+      :class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full'"
+  class="sidebar fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 lg:static lg:translate-x-0"
+>
 
     <div>
         <x-partials.sidebar-header />
