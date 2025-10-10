c703676646af57c15661dba8bf5a6096ce9d257f
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-10 22:48:42 +0800

    Refactor the code. Adding the logos component. Making the sidebar-header mobile responsive.

--- resources/views/components/partials/sidebar/sidebar-header.blade.php
+++ resources/views/components/partials/sidebar/sidebar-header.blade.php
@@ -1,16 +1,22 @@
+<div class="hidden lg:flex">
 <div 
     :class="sidebarToggle ? 'justify-center' : 'justify-between'"
     class="sidebar-header flex items-center gap-2 pt-8 pb-7">
 
     <a href="{{ route('dashboard')}}">
-        <span class="logo" :class="sidebarToggle ? 'hidden': ''">
-            <img src="{{ asset('assets/images/royalty-logo-light.png')}}" alt="logo" class="ml-3"/>
-        </span>
 
         <img 
         src={{ asset('assets/images/logo.png')}} 
         alt="logo"
-        :class="sidebarToggle ? '' : 'hidden'"/>
+        :class="sidebarToggle ? '' : 'hidden'"
+        "/>
+
+        <span class="logo" :class="sidebarToggle ? 'hidden': ''">
+        <x-partials.header.logos />
+       </span>
+
+     
     </a>
 
+</div>
 </div>
