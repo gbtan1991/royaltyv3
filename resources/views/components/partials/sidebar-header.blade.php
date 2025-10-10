eea41aeba8f601c6ac58069c4586db9b5ab0a520
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-10 20:03:51 +0800

    Added the functionality in the sidebarToggle related to the original logo.

--- resources/views/components/partials/sidebar-header.blade.php
+++ resources/views/components/partials/sidebar-header.blade.php
@@ -1,7 +1,9 @@
-<div class="sidebar-header flex items-center gap-2 pt-8 pb-7">
+<div 
+    :class="sidebarToggle ? '' : ''"
+    class="sidebar-header flex items-center gap-2 pt-8 pb-7">
 
     <a href="{{ route('dashboard')}}">
-        <span class="logo">
+        <span class="logo" :class="sidebarToggle ? 'hidden': ''">
             <img src="{{ asset('assets/images/royalty-logo-light.png')}}" alt="logo" class="ml-3"/>
         </span>
     </a>
