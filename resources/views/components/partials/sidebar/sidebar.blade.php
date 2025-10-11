8dcdf51c87641ff54a56b1de8219837ade612f53
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-11 19:29:41 +0800

    Added the transition and properly changed the toggling of sidebar

--- resources/views/components/partials/sidebar/sidebar.blade.php
+++ resources/views/components/partials/sidebar/sidebar.blade.php
@@ -1,12 +1,9 @@
 <aside
-      :class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full'"
-  class="sidebar fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 lg:static lg:translate-x-0"
+    :class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full lg:translate-x-0 lg:w-[290px]'"
+    class="sidebar fixed top-0 left-0 z-50 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 transition-all duration-300 ease-in-out
+           lg:static lg:translate-x-0"
 >
-
     <div>
         <x-partials.sidebar.sidebar-header />
-    
     </div>
-
-
 </aside>
