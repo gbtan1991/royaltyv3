1eae9080641929641bd92b9f036ffe2eac8e7ab5
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-11 19:49:05 +0800

    Fixed the synchronization of the sidebarToggle.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,19 @@
 <x-layouts.app-layout>
     <div class="max-w-3xl mx-auto mt-10 h-96">
-    <h1 class="text-2xl font-bold">Welcome, {{ auth('admin')->user()->full_name }} 🎉</h1>
+        <h1 class="text-2xl font-bold">
+            Welcome, {{ auth('admin')->user()->full_name }} 🎉
+        </h1>
 
-    <a href="{{ route('admin.index') }}" class="text-blue-600 hover:underline">Go to Admin Dashboard</a>
-    <form action="{{ route('admin.logout') }}" method="POST" class="mt-4">
-        @csrf
-        <button class="bg-red-600 text-white px-4 py-2 rounded">Logout</button>
-    </form>
-</div>
+        <a href="{{ route('admin.index') }}" class="text-blue-600 hover:underline">
+            Go to Admin Dashboard
+        </a>
+
+        <form action="{{ route('admin.logout') }}" method="POST" class="mt-4">
+            @csrf
+            <button class="bg-red-600 text-white px-4 py-2 rounded">
+                Logout
+            </button>
+        </form>
+    </div>
+
 </x-layouts.app-layout>
