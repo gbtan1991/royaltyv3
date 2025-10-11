834131ed04fd459497b9c8443f9dac7e80a04a79
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-11 18:12:52 +0800

    Created component for menuButton

---
+++ resources/views/components/partials/header/menu-button.blade.php
@@ -0,0 +1,7 @@
+<button 
+class="z-50 flex h-10 w-10 items-center justify-center rounded-lg border border-gray-200 text-gray-500 lg:h-11 lg:w-11"
+>
+
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+
+</button>
