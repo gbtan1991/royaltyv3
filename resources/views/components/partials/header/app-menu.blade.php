176ae9d6ea99aea1345942424c6fd3b10b74ee66
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-12 23:38:51 +0800

    Refactered the app-menu

--- resources/views/components/partials/header/app-menu.blade.php
+++ resources/views/components/partials/header/app-menu.blade.php
@@ -1,5 +1,7 @@
-<div class="w-full h-28">
+ <div
+        :class="menuToggle ? 'flex' : 'hidden'"
+        class="w-full items-center z-99999 justify-between gap-4 px-5 py-4 lg:flex lg:justify-end lg:px-0 lg:shadow-none"
+    >
 
-    test
-
+    <div>This is the app menu</div>
 </div>
