41634244dfee74cac9196b5f679cd6ff25fb3c81
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-12 23:39:19 +0800

    trying to refactor the sidebar

--- resources/views/components/partials/sidebar/sidebar.blade.php
+++ resources/views/components/partials/sidebar/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside
     :class="sidebarToggle ? 'translate-x-0 lg:w-[290px]' : '-translate-x-full lg:translate-x-0 lg:w-[90px]'"
-    class="sidebar fixed top-0 left-0 z-50 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 transition-all duration-300 ease-in-out
+    class="sidebar  fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 transition-all duration-300 ease-in-out
            lg:static lg:translate-x-0"
 >
     <div>
