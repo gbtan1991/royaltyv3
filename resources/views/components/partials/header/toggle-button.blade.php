e68fb5604d47855206dd4267bc4ce178936ffe95
gilbert <gilbert.tan1991@gmail.com>
2025-10-13 10:18:57 +0800

    Refactor the sizing of the width and height of the toggleButton

--- resources/views/components/partials/header/toggle-button.blade.php
+++ resources/views/components/partials/header/toggle-button.blade.php
@@ -1,5 +1,5 @@
 <button @click.stop="{{ $target }} = !{{ $target }}" :class="{{ $target }} ? '' : ''"
-    class="z-50 flex h-{{ $size }} w-{{ $size }} items-center justify-center  border {{ $borderRadius }} border-gray-200 text-gray-500 lg:h-{{ (int) $size + 1 }} lg:w-{{ (int) $size + 1 }}">
+    class="z-50 flex h-{{ $size }} w-{{ $size }} lg:h-{{ (int) $size + 1 }} lg:w-{{ (int) $size + 1 }}  items-center justify-center  border {{ $borderRadius }} border-gray-200 text-gray-500 ">
     <!-- Default Button -->
     <i class="fa-solid {{ $iconDefault }}" x-show="!{{ $target }}" x-transition></i>
 
