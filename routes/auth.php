fd58fcc1d36e8586321c8fd6bbfa2e26ba2cc3bc
gbtan23 <gilbert.tan1991@gmail.com>
2025-10-18 21:58:41 +0800

    Refactor the Auth.php

---
+++ routes/auth.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AdminAuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin Authentication Routes
+|--------------------------------------------------------------------------
+*/
+
+
+Route::middleware('guest:admin')->group(function (){
+    Route::get('admin/auth/logion', [AdminAuthController::class, 'showLoginForm'])
+    ->name('admin.login');
+
+    Route::post('admin/auth/login', [AdminAuthController::class, 'login'])
+    ->name('admin.login.submit');
+});
+
+Route::post('admin/auth/logout', [AdminAuthController::class, 'logout'])
+    ->middleware('auth:admin')
+    ->name('admin.logout');
