888f6ff7af52fcc52dab42d76044ef02e45e6683
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-22 20:43:24 +0800

    Refactor AdminFactory to define default state for Admin model attributes

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
@@ -17,7 +18,21 @@ class AdminFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'username' => fake()->unique()->userName(),
+            'password_hash' => Hash::make('password123'), // default password
+            'role' => fake()->randomElement(['superadmin', 'admin']),
+            'email' => fake()->unique()->safeEmail(),
+            'status' => fake()->randomElement(['active', 'inactive', 'locked']),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'avatar' => null,
+            'last_login_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'last_login_ip' => fake()->ipv4(),
+            'login_attempts' => fake()->numberBetween(0, 5),
+            'locked_until' => null,
+            'password_reset_token' => null,
+            'password_reset_sent_at' => null,
         ];
+
     }
 }
