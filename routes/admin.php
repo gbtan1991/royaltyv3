a4b0e164775fec458946f6c9f9698a35c82e7e36
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-26 23:08:27 +0800

    Add admin routes for CRUD operations in AdminController

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,30 @@
 <?php 
 
+use App\Http\Controllers\admin\AdminController;
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [App\Http\Controllers\admin\AdminController::class, 'index'])->name('index');
-    
 
-});
+Route::prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        // Admin index page (list of admins)
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+
+        // Show form to create new admin
+        Route::get('/create', [AdminController::class, 'create'])->name('create');
+
+        // Store new admin
+        Route::post('/', [AdminController::class, 'store'])->name('store');
+
+        // Show form to edit existing admin
+        Route::get('/{admin}/edit', [AdminController::class, 'edit'])->name('edit');
+
+        // Update existing admin
+        Route::put('/{admin}', [AdminController::class, 'update'])->name('update');
+
+        // Delete existing admin
+        Route::delete('/{admin}', [AdminController::class, 'destroy'])->name('destroy');
+
+        // Optional: show single admin (if needed)
+        Route::get('/{admin}', [AdminController::class, 'show'])->name('show');
+    });
