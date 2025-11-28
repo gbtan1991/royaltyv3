8adf8e93168cd837e74aca148ed9d338c364d738
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-28 21:05:15 +0800

    Refactor Admin model to extend Authenticatable and include Notifiable traits

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\AdminFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'username',
+        'password_hash',
+        'role',
+        'status',
+        'first_name',
+        'last_name',
+        'avatar',
+        'last_login_at',
+        'last_login_ip',
+        'login_attempts',
+        'locked_until',
+        'password_reset_token',
+        'password_reset_sent_at',
+    ];
+
+    protected $hidden = [
+        'password_hash',
+        'password_reset_token'
+    ];
+
+    public function getAuthPassword()
+    {
+        return $this->password_hash;
+    }
+
 }
