bb7db14c52fd87affc3cdd14db63b0aa3f45e382
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-28 21:04:21 +0800

    Update login route names for clarity and consistency

--- routes/auth.php
+++ routes/auth.php
@@ -2,4 +2,5 @@
 
 use App\Http\Controllers\auth\AuthController;
 
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login.show');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
