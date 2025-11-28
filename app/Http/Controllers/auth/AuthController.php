d0da3ad5e9f20840b129310a59ad75c3491585e7
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-28 20:13:57 +0800

    Add registration view and method in AuthController

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -10,4 +10,8 @@ class AuthController extends Controller
     public function showLogin() {
         return view('auth.login');
     }
+
+    public function showRegister(){
+        return view('auth.register');
+    }
 }
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.guest-layout>
+    
+</x-layouts.guest-layout>
