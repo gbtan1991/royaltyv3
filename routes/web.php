40089db9b126972c26a8543038a24be92610975b
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-30 23:43:09 +0800

    Update home route to redirect to login view and remove unused dashboard route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
-Route::get('/dashboard', function(){
-    return view('dashboard');
-})->name('dashboard');
-
 
 
 require __DIR__.'/admin.php';
