5d90463df6a84ca088d1126605a8891ed5178bd9
gbtan23 <gilbert.tan1991@gmail.com>
2025-11-30 23:42:34 +0800

    Add DashboardController and update dashboard view with logout form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,10 @@
     <h1>Admin Dashboard</h1>
 
     <x-partials.nav-bar />
-    
+
+    <form action="{{ route('logout.post') }}" method="POST">
+    @csrf
+    <button type="submit">Logout</button>
+</form>
 
 </x-layouts.app-layout>
