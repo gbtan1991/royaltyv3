cfc3f0a205d745cfbd288aca984c2cf112d9c4fc
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-02 23:03:42 +0800

    Refactor HTML structure in app layout for improved readability

--- resources/views/components/layouts/app-layout.blade.php
+++ resources/views/components/layouts/app-layout.blade.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Royalty Rewards App') }}</title>
-        
+
     <!--- Fonts --->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
     <!--- Style Scrips --->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
     <!-- favicon --->
     <link rel="icon" type="image/png" href="assets/favicon/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="assets/favicon//favicon.svg" />
-<link rel="shortcut icon" href="assets/favicon/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png" />
-<link rel="manifest" href="/site.webmanifest" />
-        
+    <link rel="icon" type="image/svg+xml" href="assets/favicon//favicon.svg" />
+    <link rel="shortcut icon" href="assets/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+
 
 </head>
+
 <body>
     {{ $slot }}
 </body>
+
 </html>
