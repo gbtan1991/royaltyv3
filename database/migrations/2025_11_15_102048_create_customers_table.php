617b77edfa324e348ec9ef9dcddd41e8eaed12b8
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-13 00:04:56 +0800

    Refactor the customers table for a better database structure.

--- database/migrations/2025_11_15_102048_create_customers_table.php
+++ database/migrations/2025_11_15_102048_create_customers_table.php
@@ -8,30 +8,20 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('customer', function (Blueprint $table) {
             $table->id();
+            $table->integer('member_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('loyalty_tier', ['bronze', 'silver', 'gold', 'platinum'])->nullable();
+            $table->timestamps();
 
-            // Identity
-            $table->string('username')->unique();
-
-            // Personal Information
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->date('birthdate')->nullable();
-            $table->enum('gender', ['male', 'female', 'other'])->default('other');
-            $table->enum('status', ['active', 'inactive', 'banned'])->default('inactive');
-
-            // Rewards / Points
-            $table->unsignedInteger('total_points')->default(0);
-
-            // Timestamps
-            $table->timestamps();           // created_at, updated_at
-            $table->softDeletes();          // deleted_at (for soft deleting)
+            // Foreign key
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('customer');
     }
 };
