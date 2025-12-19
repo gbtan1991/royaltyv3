ee0a5115305970c2c3b5e54e88d2f39ec36925d4
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-20 00:22:35 +0800

    Refactor admin list view to properly display user details and roles

--- resources/views/components/admin/admin-list.blade.php
+++ resources/views/components/admin/admin-list.blade.php
@@ -21,20 +21,45 @@
 
             </tr>
         </thead>
-        <tbody>
-           @foreach ($admins as $admin) 
-                <td><img src="{{ app(\Laravolt\Avatar\Avatar::class)->create($admin->first_name . ' ' . $admin->last_name)->toBase64() }}" 
-     class="w-10 h-10 rounded-full"/></td>
-                <td class="p-2 border">{{ $admin->id }}</td>
-                <td class="p-2 border">{{ $admin->username }}</td>
-                <td class="p-2 border">{{ $admin->first_name }} {{ $admin->last_name }}</td>
-                <td class="p-2 border">{{ $admin->role }}</td>
-                <td class="p-2 border">{{ $admin->last_login_at }}</td>
-                <td class="p-2 border">{{ $admin->locked_until }}</td>
-                <td class="p-2 border">{{ $admin->status }}</td>
-                <td class="p-2 border"><a href="{{ route('admin.show', $admin) }}" class="text-blue-600 underline">Edit</a></td>
-            </tr>
-              @endforeach
-        </tbody>
+       <tbody>
+    @foreach ($admins as $admin) 
+    <tr>
+        <td class="p-2 border">
+            <img src="{{ app(\Laravolt\Avatar\Avatar::class)->create($admin->user->first_name . ' ' . $admin->user->last_name)->toBase64() }}" 
+                 class="w-10 h-10 rounded-full"/>
+        </td>
+        
+        <td class="p-2 border">{{ $admin->user_id }}</td>
+        
+        <td class="p-2 border font-semibold">{{ $admin->username }}</td>
+
+        <td class="p-2 border">
+            {{ $admin->user->first_name }} {{ $admin->user->last_name }}
+        </td>
+
+        <td class="p-2 border">
+            <span class="px-2 py-1 bg-blue-100 text-blue-800 rounded text-xs">
+                {{ $admin->role }}
+            </span>
+        </td>
+
+        <td class="p-2 border text-sm text-gray-600">
+            {{ $admin->last_login_at ?? 'Never' }}
+        </td>
+
+        <td class="p-2 border text-sm">
+            {{ $admin->locked_until ?? 'N/A' }}
+        </td>
+
+        <td class="p-2 border">
+            {{ $admin->status }}
+        </td>
+
+        <td class="p-2 border">
+            <a href="{{ route('admin.show', $admin) }}" class="text-blue-600 hover:underline">View/Edit</a>
+        </td>
+    </tr>
+    @endforeach
+</tbody>
     </table>
 </div>
