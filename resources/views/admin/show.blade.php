77adc2a95f2bfe0d496f6ce668d2248feb89ce29
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-20 21:06:18 +0800

    Refactor admin show view to use consistent variable naming for admin details

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -7,7 +7,7 @@
         {{-- Avatar --}}
         <div class="flex justify-center mb-4">
             <img 
-                src="{{ app(\Laravolt\Avatar\Avatar::class)->create($adminProfile->first_name . ' ' . $adminProfile->last_name)->toBase64() }}"
+                src="{{ app(\Laravolt\Avatar\Avatar::class)->create($admin->first_name . ' ' . $admin->last_name)->toBase64() }}"
                 class="w-24 h-24 rounded-full"
             />
         </div>
@@ -16,43 +16,43 @@
         <table class="w-full text-left">
             <tr>
                 <th class="py-2 w-40">ID</th>
-                <td>{{ $adminProfile->id }}</td>
+                <td>{{ $admin->id }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Username</th>
-                <td>{{ $adminProfile->username }}</td>
+                <td>{{ $admin->username }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Full Name</th>
-                <td>{{ $adminProfile->user->first_name }} {{ $adminProfile->user->last_name }}</td>
+                <td>{{ $admin->user->first_name }} {{ $admin->user->last_name }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Role</th>
-                <td>{{ ucfirst($adminProfile->role) }}</td>
+                <td>{{ ucfirst($admin->role) }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Status</th>
-                <td>{{ ucfirst($adminProfile->status) }}</td>
+                <td>{{ ucfirst($admin->status) }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Last Login</th>
-                <td>{{ $adminProfile->last_login_at ?? 'Never' }}</td>
+                <td>{{ $admin->last_login_at ?? 'Never' }}</td>
             </tr>
 
             <tr>
                 <th class="py-2">Locked Until</th>
-                <td>{{ $adminProfile->locked_until ?? 'Not locked' }}</td>
+                <td>{{ $admin->locked_until ?? 'Not locked' }}</td>
             </tr>
         </table>
 
         {{-- Actions --}}
         <div class="mt-6 flex gap-2">
-            <a href="{{ route('admin.edit', $adminProfile) }}" 
+            <a href="{{ route('admin.edit', $admin) }}" 
                class="px-4 py-2 bg-blue-600 text-white rounded">
                Edit
             </a>
