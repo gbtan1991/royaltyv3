698454ea5d8b8ad3a17f631d111ae374cbf1ac36
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-20 23:07:20 +0800

    Fix indentation and formatting in admin list view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,14 @@
 <x-layouts.app-layout>
 
- <h3>List of Admins</h3>
+    <h3>List of Admins</h3>
 
 
- <a href="{{ route('admin.create')  }}">Add new Admin</a>
+    <a href="{{ route('admin.create') }}">Add new Admin</a>
+    <a href="{{ route('dashboard') }}" class="mb-4 inline-block text-blue-600 hover:underline">Back to Dashboard</a>
 
-<x-admin.admin-list :admins="$admins"/>
-    
+
+    <x-admin.admin-list :admins="$admins" />
+
+
 
 </x-layouts.app-layout>
