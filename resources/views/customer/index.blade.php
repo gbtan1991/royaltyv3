e7bb06aabe88b39edd9a472126a2838d2d9148f7
gbtan23 <gilbert.tan1991@gmail.com>
2025-12-21 21:32:31 +0800

    Added the link for the customer.create in the index.

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -4,10 +4,12 @@
 
 {{-- 
     <a href="{{ route('customer.create') }}">Add new Admin</a> --}}
-    <a href="{{ route('dashboard') }}" class="mb-4 inline-block text-blue-600 hover:underline">Back to Dashboard</a>
-
+    <a href="{{ route('dashboard') }}" class="mb-4 inline-block text-blue-600 hover:underline">Back to Dashboard</a><br>
+    <a href="{{ route("customer.create") }}" class="mb-4 inline-block text-blue-600 hover:underline">Add new Customer</a>
 
     <x-customer.customer-list :customers="$customers" />
+
+
 
 
 
