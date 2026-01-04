8070b8c014af815a8a954e38fb53ca5feb24b97e
gilbert <gilbert.tan1991@gmail.com>
2026-01-05 00:35:17 +0800

    Created the pointsledger model

--- app/Models/PointsLedger.php
+++ app/Models/PointsLedger.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PointsLedger extends Model
 {
-    //
+    protected $table = 'points_ledger';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'points_amount',
+        'source_type',
+        'source_id',
+        'description'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
