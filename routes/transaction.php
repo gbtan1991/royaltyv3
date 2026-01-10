22eef245a7517d3111754b0c63f7c3807e3521da
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-10 23:10:00 +0800

    Added the needed routes for table columns created

--- routes/transaction.php
+++ routes/transaction.php
@@ -15,6 +15,12 @@ Route::middleware('auth:admin')
     
     Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
 
+    Route::get('/show', [SalesTransactionController::class, 'show'])->name('show');
+
+    Route::get('/edit', [SalesTransactionController::class, 'edit'])->name('edit');
+
+    Route::delete('/delete', [SalesTransactionController::class, 'destroy'])->name('delete');
+
 
     });
 
