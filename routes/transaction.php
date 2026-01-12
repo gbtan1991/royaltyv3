82867aab31b03f475b48dac1b9fcc1267a64a443
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-12 22:04:33 +0800

    Refactored the routes for proper url in edit, show and delete.

--- routes/transaction.php
+++ routes/transaction.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 use App\Http\Controllers\transaction\SalesTransactionController;
@@ -6,20 +6,20 @@ use App\Http\Controllers\transaction\SalesTransactionController;
 Route::middleware('auth:admin')
     ->prefix('transaction')
     ->name('transaction.')
-    ->group(function() {
-        
+    ->group(function () {
 
-    Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
 
-    Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
-    
-    Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
+        Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
 
-    Route::get('/show', [SalesTransactionController::class, 'show'])->name('show');
+        Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
 
-    Route::get('/edit', [SalesTransactionController::class, 'edit'])->name('edit');
+        Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
 
-    Route::delete('/delete', [SalesTransactionController::class, 'destroy'])->name('delete');
+        Route::get('/{transaction}', [SalesTransactionController::class, 'show'])->name('show');
+
+        Route::get('/{transaction}/edit', [SalesTransactionController::class, 'edit'])->name('edit');
+
+        Route::delete('/{transaction}', [SalesTransactionController::class, 'destroy'])->name('destroy');
 
 
     });
