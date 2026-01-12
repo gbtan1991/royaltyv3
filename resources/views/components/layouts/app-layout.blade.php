3508d5e0f0ea54271c5e21d145255f6cf983909f
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-12 21:58:48 +0800

    Properly linked the favicon/icon

--- resources/views/components/layouts/app-layout.blade.php
+++ resources/views/components/layouts/app-layout.blade.php
@@ -14,14 +14,14 @@
     <!--- Style Scrips --->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-
+    
     <!-- favicon --->
-    <link rel="icon" type="image/png" href="assets/favicon/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="assets/favicon//favicon.svg" />
-    <link rel="shortcut icon" href="assets/favicon/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
     <link rel="manifest" href="/site.webmanifest" />
-
 
 </head>
 
