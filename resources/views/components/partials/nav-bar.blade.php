41bfe37989e31ab7a19fff7355895bb441059386
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-19 21:04:30 +0800

    Added new link for checking points ledger

--- resources/views/components/partials/nav-bar.blade.php
+++ resources/views/components/partials/nav-bar.blade.php
@@ -2,5 +2,7 @@
     <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
     <li><a href="{{ route('customer.index') }}">Manage Customer</a></li>
     <li><a href="{{ route('transaction.index') }}">Manage Transactions</a></li>
+    <li><a href="{{ route('transaction.ledger.index') }}">Check points</a></li>
     <li><a href="{{ route('admin.index') }}">Manage Admin</a></li>
+    
 </ul>
