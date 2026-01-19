3382518d0327d468e4ed7369bbaa8732114ea3c0
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-19 21:01:23 +0800

    Added a new route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware(['auth:admin'])->name('dashboard');
+
+
 
 require __DIR__.'/admin.php';
 require __DIR__.'/auth.php';
