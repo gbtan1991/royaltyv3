08ea1769b843d59b3cac8998dff58126a18f9527
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-19 21:03:32 +0800

    Added new route for ledger index

--- routes/transaction.php
+++ routes/transaction.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\transaction\PointsLedgerController;
 use App\Http\Controllers\transaction\SalesTransactionController;
 
 Route::middleware('auth:admin')
@@ -9,19 +10,19 @@ Route::middleware('auth:admin')
     ->group(function () {
 
 
+        Route::get('/ledger', [PointsLedgerController::class, 'index'])->name('ledger.index');
         Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
-
         Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
 
+
+
         Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
-
         Route::get('/{transaction}', [SalesTransactionController::class, 'show'])->name('show');
-
         Route::get('/{transaction}/edit', [SalesTransactionController::class, 'edit'])->name('edit');
-
-        Route::put('/{transaction}/update', [SalesTransactionController::class, 'update'])->name('update');
+        Route::put('/{transaction}', [SalesTransactionController::class, 'update'])->name('update');
 
         Route::delete('/{transaction}', [SalesTransactionController::class, 'destroy'])->name('destroy');
+
 
 
     });
