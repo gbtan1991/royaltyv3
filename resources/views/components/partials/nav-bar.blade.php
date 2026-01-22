bba3595f84fcc23b4944fd68ddfc7e5dcb642781
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-22 22:48:36 +0800

    Added the new links for the new modules in the nav-bar

--- resources/views/components/partials/nav-bar.blade.php
+++ resources/views/components/partials/nav-bar.blade.php
@@ -3,6 +3,9 @@
     <li><a href="{{ route('customer.index') }}">Manage Customer</a></li>
     <li><a href="{{ route('transaction.index') }}">Manage Transactions</a></li>
     <li><a href="{{ route('transaction.ledger.index') }}">Check points</a></li>
+    <li><a href="{{ route('rewards.index') }}">Manage Rewards</a></li>
     <li><a href="{{ route('admin.index') }}">Manage Admin</a></li>
+    <li><a href="{{ route('settings.index') }}">Settings</a></li>
+    
     
 </ul>
