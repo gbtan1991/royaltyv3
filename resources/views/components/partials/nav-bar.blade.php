8ca0cd751b7311b5dac73bbfb40c40ace548815f
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-23 22:24:23 +0800

    Added the redemption link for viewing all claims

--- resources/views/components/partials/nav-bar.blade.php
+++ resources/views/components/partials/nav-bar.blade.php
@@ -4,6 +4,7 @@
     <li><a href="{{ route('transaction.index') }}">Manage Transactions</a></li>
     <li><a href="{{ route('transaction.ledger.index') }}">Check points</a></li>
     <li><a href="{{ route('rewards.index') }}">Manage Rewards</a></li>
+    <li><a href="{{ route('redemption.index') }}">All of the Claims</a></li>
     <li><a href="{{ route('admin.index') }}">Manage Admin</a></li>
     <li><a href="{{ route('settings.index') }}">Settings</a></li>
     
