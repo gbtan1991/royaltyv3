59e2d5d8009aa010f6a1e0997803b6d5b072820f
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-24 22:01:32 +0800

    Update the namespace of UpdateAdminRequest

--- app/Http/Requests/admin/UpdateAdminRequest.php
+++ app/Http/Requests/admin/UpdateAdminRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\admin;
 
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
@@ -12,6 +12,7 @@ class UpdateAdminRequest extends FormRequest
      */
     public function authorize(): bool
     {
+        
         return $this->user()?->role === 'Super Admin';
 
     }
