580a4c60a0570dc88aec4d093b23a4a0bd8b00f1
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-24 22:03:36 +0800

    Refactored the transaction route to sue Route::resource and also exclude the separated specific routes.

--- routes/transaction.php
+++ routes/transaction.php
@@ -5,31 +5,53 @@ use App\Http\Controllers\transaction\RedemptionController;
 use App\Http\Controllers\transaction\PointsLedgerController;
 use App\Http\Controllers\transaction\SalesTransactionController;
 
-Route::middleware('auth:admin')
-    ->prefix('transaction')
-    ->name('transaction.')
-    ->group(function () {
+
+Route::prefix('transaction')->name('transaction.')->group(function () {
+
+    // 1. Specific route FIRST
+    Route::get('/ledger', [PointsLedgerController::class, 'index'])->name('ledger.index');
+
+    // 2. Resource (Dynamic/Wildcard) LAST
+    Route::resource('/', SalesTransactionController::class)
+        ->parameters(['' => 'transaction']); 
+
+});
+
+Route::prefix('redemption')->name('redemption.')->group(function () {
+    // This makes the URL: /redemption
+    Route::get('/', [RedemptionController::class, 'index'])->name('index');
+
+    // This makes the URL: /redemption/create
+    Route::get('/create', [RedemptionController::class, 'create'])->name('create');
+
+    // This makes the URL: /redemption/claim
+    Route::post('/claim', [RedemptionController::class, 'store'])->name('store');
+});
+
+// Route::middleware('auth:admin')
+//     ->prefix('transaction')
+//     ->name('transaction.')
+//     ->group(function () {
+
+//         Route::get('/ledger', [PointsLedgerController::class, 'index'])->name('ledger.index');
+//         Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
+//         Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
 
 
-        Route::get('/ledger', [PointsLedgerController::class, 'index'])->name('ledger.index');
-        Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
-        Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
 
+//         Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
+//         Route::get('/{transaction}', [SalesTransactionController::class, 'show'])->name('show');
+//         Route::get('/{transaction}/edit', [SalesTransactionController::class, 'edit'])->name('edit');
+//         Route::put('/{transaction}', [SalesTransactionController::class, 'update'])->name('update');
 
-
-        Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
-        Route::get('/{transaction}', [SalesTransactionController::class, 'show'])->name('show');
-        Route::get('/{transaction}/edit', [SalesTransactionController::class, 'edit'])->name('edit');
-        Route::put('/{transaction}', [SalesTransactionController::class, 'update'])->name('update');
-
-        Route::delete('/{transaction}', [SalesTransactionController::class, 'destroy'])->name('destroy');
+//         Route::delete('/{transaction}', [SalesTransactionController::class, 'destroy'])->name('destroy');
 
        
-    });
+//     });
 
-// Keep these outside the group to avoid the "transaction." prefix
-Route::middleware('auth:admin')->group(function () {
-    Route::get('/redemption', [RedemptionController::class, 'index'])->name('redemption.index');
-    Route::get('/redemption/create', [RedemptionController::class, 'create'])->name('redemption.create');
-    Route::post('/redemption/claim', [RedemptionController::class, 'store'])->name('redemption.store');
-});
+// // Keep these outside the group to avoid the "transaction." prefix
+// Route::middleware('auth:admin')->group(function () {
+//     Route::get('/redemption', [RedemptionController::class, 'index'])->name('redemption.index');
+//     Route::get('/redemption/create', [RedemptionController::class, 'create'])->name('redemption.create');
+//     Route::post('/redemption/claim', [RedemptionController::class, 'store'])->name('redemption.store');
+// });
