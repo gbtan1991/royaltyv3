a2389225e17d652fc7c8386b6ca99d3a43955d56
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-24 22:02:56 +0800

    Refactored the customer routes to use Route:resource

--- routes/customer.php
+++ routes/customer.php
@@ -2,25 +2,13 @@
 
 use App\Http\Controllers\customer\CustomerController;
 
-Route::middleware('auth:admin')
-    ->prefix('customer')
-    ->name('customer.')
-    ->group(function() {
+Route::prefix('customer')->name('customer.')->group(function () {
 
 
-        Route::get('/customer', [CustomerController::class, 'index'])->name('index');
+    Route::resource('/', CustomerController::class)
+        ->parameters(['' => 'customer']); // Forces the variable in Controller to be $customer
 
-        Route::get('/customer/create', [CustomerController::class, 'create'])->name('create');
 
-        Route::post('/customer', [CustomerController::class, 'store'])->name('store');
 
-        Route::get('/{customer}', [CustomerController::class, 'show'])->name('show');
-
-        Route::get('/{customer}/edit', [CustomerController::class, 'edit'])->name('edit');
-
-        Route::put('/{customer}', [CustomerController::class, 'update'])->name('update'); 
-        
-        Route::delete('/{customer}', [CustomerController::class, 'destroy'])->name('destroy');
-
-    });
+});
 
