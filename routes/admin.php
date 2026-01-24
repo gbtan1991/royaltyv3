d5e79836800af519400626cc854d924711e97bfe
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-24 22:02:33 +0800

    Refactored the routes to make this using  Route::resource

--- routes/admin.php
+++ routes/admin.php
@@ -1,30 +1,13 @@
-<?php 
+<?php
 
 use App\Http\Controllers\admin\AdminController;
 
-Route::middleware('auth:admin')   // << apply alias here
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
 
-        // Admin index page (list of admins)
-        Route::get('/', [AdminController::class, 'index'])->name('index');
+Route::resource('/', AdminController::class)
+        ->parameters(['' => 'admin']); // Forces the variable in Controller to be $admin
 
-        // Show form to create new admin
-        Route::get('/create', [AdminController::class, 'create'])->name('create');
 
-        // Store new admin
-        Route::post('/', [AdminController::class, 'store'])->name('store');
 
-        // Optional: show single admin (if needed)
-        Route::get('/{admin}', [AdminController::class, 'show'])->name('show');
+});
 
-        // Show form to edit existing admin
-        Route::get('/{admin}/edit', [AdminController::class, 'edit'])->name('edit');
-
-        // Update existing admin
-        Route::put('/{admin}', [AdminController::class, 'update'])->name('update');
-
-        // Delete existing admin
-        Route::delete('/{admin}', [AdminController::class, 'destroy'])->name('destroy');
-    });
