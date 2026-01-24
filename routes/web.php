dbda5c86506e28d8877ffae720ba2de07b88226c
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-24 22:04:09 +0800

    Refactored the Web.php route to make it middleware centric.

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 1. Public routes
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function() {
-    return view('dashboard');
-})->middleware(['auth:admin'])->name('dashboard');
+// 2. Protected admin routes
+Route::middleware(['auth:admin'])->group(function () {
+
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+    // Load sub-routes files
+    require __DIR__ . '/admin.php';
+    require __DIR__ . '/customer.php';
+    require __DIR__ . '/transaction.php';
 
 
 
-require __DIR__.'/admin.php';
-require __DIR__.'/auth.php';
-require __DIR__.'/customer.php';
-require __DIR__.'/transaction.php';
-require __DIR__.'/settings.php';
-require __DIR__.'/rewards.php';
+});
+
+
+require __DIR__ . '/auth.php';
+
+
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/rewards.php';
