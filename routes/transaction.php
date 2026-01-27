49be1b1eb7ae1a5c6b6f94d080d85454d65e5c9f
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-27 22:12:54 +0800

    Clean up the comments in routes I've done last time.

--- routes/transaction.php
+++ routes/transaction.php
@@ -26,32 +26,6 @@ Route::prefix('redemption')->name('redemption.')->group(function () {
 
     // This makes the URL: /redemption/claim
     Route::post('/claim', [RedemptionController::class, 'store'])->name('store');
+
+    Route::get('/{id}', [RedemptionController::class, 'show'])->name('show');
 });
-
-// Route::middleware('auth:admin')
-//     ->prefix('transaction')
-//     ->name('transaction.')
-//     ->group(function () {
-
-//         Route::get('/ledger', [PointsLedgerController::class, 'index'])->name('ledger.index');
-//         Route::get('/', [SalesTransactionController::class, 'index'])->name('index');
-//         Route::get('/create', [SalesTransactionController::class, 'create'])->name('create');
-
-
-
-//         Route::post('/store', [SalesTransactionController::class, 'store'])->name('store');
-//         Route::get('/{transaction}', [SalesTransactionController::class, 'show'])->name('show');
-//         Route::get('/{transaction}/edit', [SalesTransactionController::class, 'edit'])->name('edit');
-//         Route::put('/{transaction}', [SalesTransactionController::class, 'update'])->name('update');
-
-//         Route::delete('/{transaction}', [SalesTransactionController::class, 'destroy'])->name('destroy');
-
-       
-//     });
-
-// // Keep these outside the group to avoid the "transaction." prefix
-// Route::middleware('auth:admin')->group(function () {
-//     Route::get('/redemption', [RedemptionController::class, 'index'])->name('redemption.index');
-//     Route::get('/redemption/create', [RedemptionController::class, 'create'])->name('redemption.create');
-//     Route::post('/redemption/claim', [RedemptionController::class, 'store'])->name('redemption.store');
-// });
