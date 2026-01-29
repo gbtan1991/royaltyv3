840197f44847959a29987523061cedad5b4360f3
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-29 21:34:15 +0800

    Created the layout of header.

---
+++ resources/views/components/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header class="sticky top-0 z-99999 flex w-full border-gray-200 bg-white lg:border-b ">
+    <div class="flex grow flex-col items-center justify-between lg:flex-row lg:px-6">
+        <div class="flex w-full items-center justify-between gap-2 border-b border-gray-200 px-3 py-3 sm:gap lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nulla ipsam autem, alias minima pariatur placeat dolores. Est ratione, beatae ab incidunt ipsum voluptas asperiores expedita, repellat vel tempore dolor.
+
+        </div>
+    </div>
+
+    
+</header>
