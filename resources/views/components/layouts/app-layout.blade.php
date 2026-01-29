4861b373f17487e976bf76df04ec7a21bd5320af
gbtan23 <gilbert.tan1991@gmail.com>
2026-01-29 21:34:05 +0800

    refactored the app layout that will be including sidebar and header.

--- resources/views/components/layouts/app-layout.blade.php
+++ resources/views/components/layouts/app-layout.blade.php
@@ -14,18 +14,31 @@
     <!--- Style Scrips --->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    
+
     <!-- favicon --->
-   <link rel="icon" type="image/png" href="{{ asset('assets/favicon/favicon-96x96.png') }}" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="{{ asset('assets/favicon/favicon.svg') }}" />
-<link rel="shortcut icon" href="{{ asset('assets/favicon/favicon.ico') }}" />
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}" />
-<link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/favicon/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('assets/favicon/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/favicon/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}" />
+    <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}" />
 
 </head>
 
 <body>
-    {{ $slot }}
+
+    <div class="flex h-screen overflow-hidden">
+        @include('components.partials.sidebar')
+
+        <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
+            @include('components.partials.header')
+
+            {{ $slot }}
+        </div>
+
+    </div>
+
+
+
 </body>
 
 </html>
