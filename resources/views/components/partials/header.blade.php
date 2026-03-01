08df1527a1e39ca788d05bb88194e7c214d5d90c
gilbert <gilbert.tan1991@gmail.com>
2026-03-02 02:14:10 +0800

    Added bg in the header for dark mode.

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header 
     x-data="{appMenuToggle: false}"
-class="sticky top-0 z-99999 flex w-full border-gray-200 bg-white lg:border-b ">
+class="sticky top-0 z-99999 flex w-full border-gray-200 bg-white lg:border-b dark:bg-gray-900">
     <div class="flex grow flex-col items-center justify-between lg:flex-row lg:px-6">
         <div
             class="flex w-full items-center justify-between gap-2 border-b border-gray-200 px-3 py-3 sm:gap lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
